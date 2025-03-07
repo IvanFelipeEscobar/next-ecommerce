1884a4906bc1190c60671665d7177f0fa5a75653
Ivan Escobar <ivnescbr1989@gmail.com>
2025-03-07 14:52:38 -0500

    finish navbar || [feat]: darkmode added

--- components/navbar/LinksDropdown.tsx
+++ components/navbar/LinksDropdown.tsx
@@ -1,6 +1,33 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  // DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+import { links } from '@/lib/utils'
+import Link from 'next/link'
+import { LuAlignLeft } from 'react-icons/lu'
+import { Button } from '../ui/button'
+
 const LinksDropdown = () => {
   return (
-    <div>LinksDropdown</div>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant={'outline'} className="flex gap-4 max-w-[100px]">
+          <LuAlignLeft className="size-6" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className='w-40' align='start' sideOffset={10}>
+        {links.map( link => (
+          <DropdownMenuItem key={link.href}>
+            <Link href={link.href} className='capitalize w-full'>
+            {link.label}
+            </Link>
+          </DropdownMenuItem>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
   )
 }
 export default LinksDropdown
--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,17 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+type NavLink = {
+  href: string
+  label: string
+}
+
+export const links: NavLink[] = [
+  { href: '/', label: 'home' },
+  { href: '/about', label: 'about' },
+  { href: '/products', label: 'products' },
+  { href: '/favorites', label: 'favorites' },
+  { href: '/cart', label: 'cart' },
+  { href: '/orders', label: 'orders' },
+]
